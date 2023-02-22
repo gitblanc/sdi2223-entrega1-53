6b8b36daec0c1c16db59e70a652658e1d48fbd85
gitblanc <UO285176@uniovi.es>
2023-02-22 17:22:22 +0100

    Repositorio inicializado

---
+++ src/main/java/com/uniovi/sdi2223entrega153/Sdi2223Entrega153Application.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2223entrega153;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2223Entrega153Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2223Entrega153Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniovi/sdi2223entrega153/Sdi2223Entrega153ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2223entrega153;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223Entrega153ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
