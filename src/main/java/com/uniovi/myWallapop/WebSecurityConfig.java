88653b015c09780799fe80ce51f501f95b6ec200
gitblanc <UO285176@uniovi.es>
2023-03-10 18:20:57 +0100

    corregidos errores de seguridad por roles de usuario. CAmbio de versión de Spring Security >>>> v5

--- src/main/java/com/uniovi/myWallapop/WebSecurityConfig.java
+++ src/main/java/com/uniovi/myWallapop/WebSecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
