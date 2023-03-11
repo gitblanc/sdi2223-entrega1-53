e6304de806849eff7ead202998c87388c8bfb4fb
Aaron <Aaron@DESKTOP-8HG00E2>
2023-03-11 13:22:33 +0100

    Commit cambios

--- src/main/java/com/uniovi/myWallapop/WebSecurityConfig.java
+++ src/main/java/com/uniovi/myWallapop/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/css/**", "/images/**", "/script/**", "/", "/signup", "/login/**").permitAll()
+                .antMatchers("/css/**", "/images/**", "/script/**", "/").permitAll()
                 .antMatchers("/user/userslist").hasAuthority("ROLE_ADMIN")
                 .antMatchers("/offer/**").hasAuthority("ROLE_STANDARD")
                 .anyRequest().authenticated()
