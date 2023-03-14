6230f1dce5cf700eefc5e525a3c7f11d8d0161fe
Aaron <Aaron@DESKTOP-8HG00E2>
2023-03-14 11:07:08 +0100

    Commit solucion logout

--- src/main/java/com/uniovi/myWallapop/WebSecurityConfig.java
+++ src/main/java/com/uniovi/myWallapop/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.uniovi.myWallapop;
 
+import com.uniovi.myWallapop.entities.Log;
+import com.uniovi.myWallapop.services.LogsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,14 +11,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    @Autowired
+    private LogsService logger;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/signup","/login").not().authenticated()
+                .antMatchers("/signup", "/login-error").not().authenticated()
                 .antMatchers("/css/**", "/images/**", "/script/**", "/").permitAll()
                 .antMatchers("/user/userslist").hasAuthority("ROLE_ADMIN")
                 .antMatchers("/offer/**").hasAuthority("ROLE_STANDARD")
@@ -26,7 +34,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login").permitAll().defaultSuccessUrl("/default")
                 .failureUrl("/login-error")
                 .and()
-                .logout().permitAll();
+                .logout().addLogoutHandler((request, response, authentication) ->
+                        {
+                            logger.addLog(new Log(Log.Tipo.LOGOUT, "Usuario ha cerrado sesión"));
+                        }
+                ).permitAll();
     }
 
     @Bean
