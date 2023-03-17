eb809a57581040a6e47e1a3e8f152611527d2a91
Santiago <uo277369@uniovi.es>
2023-03-17 17:05:01 +0100

    Arreglados fallos de pruebas

--- src/main/java/com/uniovi/myWallapop/WebSecurityConfig.java
+++ src/main/java/com/uniovi/myWallapop/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/signup", "/login-error").not().authenticated()
+                .antMatchers("/signup", "/login").not().authenticated()
                 .antMatchers("/css/**", "/images/**", "/script/**", "/").permitAll()
                 .antMatchers("/user/userslist").hasAuthority("ROLE_ADMIN")
                 .antMatchers("/offer/**").hasAuthority("ROLE_STANDARD")
