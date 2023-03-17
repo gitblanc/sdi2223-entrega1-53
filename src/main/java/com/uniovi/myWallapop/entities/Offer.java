a59c0aef30ebcbf73dd901307280e49220e46fc2
Aaron <Aaron@DESKTOP-8HG00E2>
2023-03-17 17:13:55 +0100

    Commit con comentarios de user y offer

--- src/main/java/com/uniovi/myWallapop/entities/Offer.java
+++ src/main/java/com/uniovi/myWallapop/entities/Offer.java
@@ -46,10 +46,10 @@ public class Offer {
      * Constructor que recibe los parámetros del título,
      * descripción, el precio de la oferta y el usuario que realiza
      * la oferta
-     * @param title
-     * @param description
-     * @param amount
-     * @param seller
+     * @param title String
+     * @param description String
+     * @param amount Double
+     * @param seller User
      */
     public Offer(String title, String description, Double amount, User seller) {
         this.title = title;
@@ -62,7 +62,7 @@ public class Offer {
 
     /**
      * Retorna el valor del atributo id
-     * @return
+     * @return Long
      */
     public Long getId() {
         return id;
@@ -71,7 +71,7 @@ public class Offer {
 
     /**
      * Cambia el valor del atributo id
-     * @param id
+     * @param id Long
      */
     public void setId(Long id) {
         this.id = id;
@@ -80,7 +80,7 @@ public class Offer {
 
     /**
      * Cambia el valor del atributo title
-     * @return
+     * @return String
      */
     public String getTitle() {
         return title;
@@ -89,7 +89,7 @@ public class Offer {
 
     /**
      * Cambia el valor del atributo title
-     * @param title
+     * @param title String
      */
     public void setTitle(String title) {
         this.title = title;
@@ -98,7 +98,7 @@ public class Offer {
 
     /**
      * Retorna el valor del atributo description
-     * @return
+     * @return String
      */
     public String getDescription() {
         return description;
@@ -107,7 +107,7 @@ public class Offer {
 
     /**
      * Cambia el valor de atributo description
-     * @param description
+     * @param description String
      */
     public void setDescription(String description) {
         this.description = description;
@@ -116,7 +116,7 @@ public class Offer {
 
     /**
      * Retorna el valor del atributo amount
-     * @return
+     * @return Double
      */
     public Double getAmount() {
         return amount;
@@ -125,7 +125,7 @@ public class Offer {
 
     /**
      * Cambia el valor del atributo amount
-     * @param amount
+     * @param amount Double
      */
     public void setAmount(Double amount) {
         this.amount = amount;
@@ -134,7 +134,7 @@ public class Offer {
 
     /**
      * Retorna el valor del atributo date
-     * @return
+     * @return Date
      */
     public Date getDate() {
         return date;
@@ -143,7 +143,7 @@ public class Offer {
 
     /**
      * Cambia el valor del atributo date
-     * @param date
+     * @param date Date
      */
     public void setDate(Date date) {
         this.date = date;
@@ -152,7 +152,7 @@ public class Offer {
 
     /**
      * Comprueba si la oferta está vendida o no
-     * @return
+     * @return boolean
      */
     public boolean isSold() {
         return sold;
@@ -161,7 +161,7 @@ public class Offer {
 
     /**
      * Cambia si la oferta está vendida o no
-     * @param sold
+     * @param sold boolean
      */
     public void setSold(boolean sold) {
         this.sold = sold;
@@ -170,7 +170,7 @@ public class Offer {
 
     /**
      * Devuelve el valor del atributo seller
-     * @return
+     * @return User
      */
     public User getSeller() {
         return seller;
@@ -179,7 +179,7 @@ public class Offer {
 
     /**
      * Cambia el valor del atributo seller
-     * @param seller
+     * @param seller User
      */
     public void setSeller(User seller) {
         this.seller = seller;
@@ -188,7 +188,7 @@ public class Offer {
 
     /**
      * Devuelve el valor del atributo buyer
-     * @return
+     * @return User
      */
     public User getBuyer() {
         return buyer;
@@ -197,7 +197,7 @@ public class Offer {
 
     /**
      * Cambia el valor del atributo buyer
-     * @param buyer
+     * @param buyer User
      */
     public void setBuyer(User buyer) {
         this.buyer = buyer;
@@ -206,7 +206,7 @@ public class Offer {
 
     /**
      * Devuelve el valor del atributo chats
-     * @return
+     * @return Set<Chat>
      */
     public Set<Chat> getChats() {
         return chats;
@@ -215,7 +215,7 @@ public class Offer {
 
     /**
      * Cambia el valor del atributo chats
-     * @param chats
+     * @param chats Set<Chat>
      */
     public void setChats(Set<Chat> chats) {
         this.chats = chats;
@@ -224,7 +224,7 @@ public class Offer {
 
     /**
      * Comprueba si está comprada
-     * @return
+     * @return boolean
      */
     public boolean isBuy() {
         return buy;
@@ -233,7 +233,7 @@ public class Offer {
 
     /**
      * Cambia el valor del atributo buy
-     * @param buy
+     * @param buy boolean
      */
     public void setBuy(boolean buy) {
         this.buy = buy;
@@ -242,8 +242,8 @@ public class Offer {
 
     /**
      * Método equals de Offer
-     * @param o
-     * @return
+     * @param o Object
+     * @return boolean
      */
     @Override
     public boolean equals(Object o) {
@@ -266,7 +266,7 @@ public class Offer {
 
     /**
      * Método hashCode  de Offer
-     * @return
+     * @return int
      */
     @Override
     public int hashCode() {
@@ -284,7 +284,7 @@ public class Offer {
 
     /**
      * Método toString de Offer
-     * @return
+     * @return String
      */
     @Override
     public String toString() {
